6fef42d2032ebae4610cd8e8b0d3a8bffc2aba54
Paul Saunders <coruscateor@users.noreply.github.com>
2024-06-21 18:35:58 +1200

    I setup a basic WebSocket client in WebSocketActorState.

--- src/actors/web_socket_actor_message.rs
+++ src/actors/web_socket_actor_message.rs
@@ -14,10 +14,20 @@ pub enum WebSocketActorFormat
 
 //static CONNECTION_FAILED_MESSAGE: &str = "Connection Faild";
 
+
+
+//A request is sent to the WebSocket actor to connect to a server.
+
+//The WebSocket actor closes its current connection, if it has one, and attempts to connect to the server at the provided address.
+
+//The WebSocket actor sends a unit via the provided sender to the actor-client to indicate that the message has been acknowledged and that a new connection to the server at the provided address is being made.
+
+//The WebSocket actor then communicates its connection ptogress and the connected servers output via its provided output queue.
+
 pub enum WebSocketActorInputMessage
 {
 
-    ConnectTo(String, Sender<()>) //URL, Has the WebSockect actor started trying to connect to the server?
+    ConnectTo(String) //, Sender<()>) //URL, Has the WebSockect actor started trying to connect to the server?
 
 }
 
